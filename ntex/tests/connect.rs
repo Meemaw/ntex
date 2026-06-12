Meemaw/ntex

commit 6b807efa0aa6b6d454e453ccf2a01d6beb68a543

Add support for vectorized writes (#838)

ntex-net/src/uring/stream.rs
@@ -1,7 +1,7 @@
 use std::{cell::Cell, io, mem, num::NonZeroU32, os::fd::AsRawFd, rc::Rc, task::Poll};
 
-use ntex_bytes::{Buf, BufMut, BytesMut};
-use ntex_io::IoContext;
+use ntex_bytes::{BufMut, BytePage, BytePages};
+use ntex_io::{Buffer, IoContext};
 use ntex_io_uring::{cqueue, opcode, opcode2, types::Fd};
 use ntex_rt::Arbiter;
 use ntex_util::channel::pool;
@@ -58,11 +58,11 @@ struct StreamItem {
 enum Operation {
     Recv {
         id: usize,
-        buf: BytesMut,
+        buf: Buffer,
     },
     Send {
         id: usize,
-        buf: BytesMut,
+        buf: BytePage,
         result: Option<io::Result<usize>>,
     },
     Poll {
@@ -188,7 +188,7 @@ impl Handler for StreamOpsHandler {
                         log::trace!("{}: Recv canceled {:?}", item.tag(), item.fd());
                         item.rd_op.take();
                         item.flags.remove(Flags::RD_CANCELING);
-                        item.ctx.release_read_buf(0, buf, Poll::Pending);
+                        item.ctx.release_read_buf(buf, Poll::Pending);
                         if item.flags.contains(Flags::RD_REISSUE) {
                             item.flags.remove(Flags::RD_REISSUE);
                             st.recv(id, false, &self.inner.api);
@@ -198,9 +198,10 @@ impl Handler for StreamOpsHandler {
                 Operation::Send { id, buf, .. } => {
                     if let Some(item) = st.streams.get_mut(id) {
                         log::trace!("{}: Send canceled: {:?}", item.tag(), item.fd());
+                        item.ctx.with_write_buf(|pages| pages.prepend(buf));
                         item.wr_op.take();
                         item.flags.remove(Flags::WR_CANCELING);
-                        item.ctx.release_write_buf(buf, Poll::Pending);
+                        item.ctx.update_write_buf(Poll::Pending);
                         if item.flags.contains(Flags::WR_REISSUE) {
                             item.flags.remove(Flags::WR_REISSUE);
                             st.send(id, &self.inner.api);
@@ -245,7 +246,7 @@ impl Handler for StreamOpsHandler {
                                 st.recv_more(id, buf, &self.inner.api);
                             } else {
                                 item.flags.remove(Flags::RD_MORE);
-                                if item.ctx.release_read_buf(total, buf, res).ready() {
+                                if item.ctx.release_read_buf(buf, res).ready() {
                                     st.recv(id, self.inner.api.is_new(), &self.inner.api);
                                 }
                             }
@@ -255,7 +256,7 @@ impl Handler for StreamOpsHandler {
                 Operation::Send { id, buf, result } => {
                     if let Some(item) = st.streams.get_mut(id) {
                         if cqueue::notif(flags) {
-                            if item.ctx.release_write_buf(buf, Poll::Ready(result.unwrap())).ready() {
+                            if item.ctx.update_write_buf(Poll::Ready(result.unwrap())).ready() {
                                 st.send(id, &self.inner.api);
                             }
                         } else if cqueue::more(flags) {
@@ -274,7 +275,7 @@ impl Handler for StreamOpsHandler {
                             item.wr_op.take();
 
                             // release buffer and try to send next chunk
-                            if item.ctx.release_write_buf(buf, Poll::Ready(res)).ready() {
+                            if item.ctx.update_write_buf(Poll::Ready(res)).ready() {
                                 st.send(id, &self.inner.api);
                             }
                         }
@@ -350,7 +351,7 @@ impl StreamOpsStorage {
         }
     }
 
-    fn recv_more(&mut self, id: usize, mut buf: BytesMut, api: &DriverApi) {
+    fn recv_more(&mut self, id: usize, mut buf: Buffer, api: &DriverApi) {
         if let Some(item) = self.streams.get_mut(id) {
             item.ctx.resize_read_buf(&mut buf);
 
@@ -369,10 +370,10 @@ impl StreamOpsStorage {
     fn send(&mut self, id: usize, api: &DriverApi) {
         if let Some(item) = self.streams.get_mut(id) {
             if item.wr_op.is_none() {
-                if let Some(buf) = item.ctx.get_write_buf() {
-                    let slice = buf.chunk();
-                    let buf_ptr = slice.as_ptr();
-                    let buf_len = slice.len() as u32;
+                let page = item.ctx.with_write_buf(BytePages::take);
+                if let Some(buf) = page {
+                    let buf_ptr = buf.as_ptr();
+                    let buf_len = buf.len() as u32;
                     let op_id = self.ops.insert(Some(Operation::Send {
                         id,
                         buf,

ntex/tests/connect.rs
@@ -39,16 +39,7 @@ async fn test_openssl_string() {
     let mut tcp = Some(tcp);
     let srv = build_test_server(async move |srv| {
         srv.listen("test", tcp.take().unwrap(), async |_| {
-            chain_factory(
-                fn_service(|io: Io<_>| async move {
-                    let res = io.read_ready().await;
-                    assert!(res.is_ok());
-                    Ok(io)
-                })
-                .map_init_err(|_| ()),
-            )
-            .and_then(openssl::SslAcceptor::new(ssl_acceptor()))
-            .and_then(
+            chain_factory(openssl::SslAcceptor::new(ssl_acceptor())).and_then(
                 fn_service(|io: Io<_>| async move {
                     io.send(Bytes::from_static(b"test"), &BytesCodec)
                         .await
@@ -69,7 +60,7 @@ async fn test_openssl_string() {
 
     // ssl connector
     let conn = ntex::connect::openssl::SslConnector::new(connector.clone())
-        .pipeline(SharedCfg::default())
+        .pipeline(SharedCfg::new("CLIENT").into())
         .await
         .unwrap();
     let addr = format!("127.0.0.1:{}", srv.addr().port());
@@ -90,7 +81,7 @@ async fn test_openssl_string() {
 
     // ssl connector 2
     let conn = ntex::connect::openssl::SslConnector2::new(connector)
-        .pipeline(SharedCfg::default())
+        .pipeline(SharedCfg::new("CLIENT").into())
         .await
         .unwrap();
     let addr = format!("127.0.0.1:{}", srv.addr().port());
@@ -132,15 +123,7 @@ async fn test_openssl_read_before_error() {
     use tls_openssl::ssl::{SslConnector, SslMethod, SslVerifyMode};
 
     let srv = test_server(async || {
-        chain_factory(
-            fn_service(async move |io: Io<_>| {
-                let _ = io.read_ready().await;
-                Ok(io)
-            })
-            .map_init_err(|_| ()),
-        )
-        .and_then(openssl::SslAcceptor::new(ssl_acceptor()))
-        .and_then(
+        chain_factory(openssl::SslAcceptor::new(ssl_acceptor())).and_then(
             fn_service(|io: Io<_>| async move {
                 io.send(Bytes::from_static(b"test"), &Rc::new(BytesCodec))
                     .await
@@ -354,7 +337,6 @@ async fn test_create() {
     assert_eq!(io.query::<PeerAddr>().get().unwrap(), srv.addr().into());
 }
 
-#[cfg(feature = "openssl")]
 #[ntex::test]
 async fn test_uri() {
     let srv = test_server(async || {
@@ -365,6 +347,7 @@ async fn test_uri() {
             Ok::<_, io::Error>(())
         })
     });
+    time::sleep(time::Millis(50)).await;
 
     let conn = Pipeline::new(ntex::connect::ConnectorService::default());
     let addr =

ntex-net/src/polling/stream.rs
@@ -393,20 +393,34 @@ impl StreamItem {
     }
 
     fn write(&mut self) -> IoTaskStatus {
-        if let Some(buf) = self.ctx.get_write_buf() {
-            let fd = self.fd();
-            log::trace!("{}: {fd:?}-Wrt buf({:?})", self.ctx.tag(), buf.len());
-            let res = syscall!(break libc::write(fd, buf[..].as_ptr().cast(), buf.len()));
-            return self.ctx.release_write_buf(buf, res);
+        let res = self.ctx.with_write_buf(|buf| {
+            if let Some(mut page) = buf.take() {
+                let fd = self.fd();
+                log::trace!("{}: {fd:?}-Wrt buf({:?})", self.ctx.tag(), page.len());
+                let res = syscall!(break libc::write(fd, page.as_ptr().cast(), page.len()));
+                println!("{}: {res:?}", self.ctx.tag());
+                if let Poll::Ready(Ok(n)) = res
+                    && page.len() != n
+                {
+                    page.advance_to(n);
+                    buf.prepend(page);
+                }
+                Some(res)
+            } else {
+                None
+            }
+        });
+        if let Some(res) = res {
+            self.ctx.update_write_buf(res)
+        } else {
+            IoTaskStatus::Pause
         }
-        IoTaskStatus::Pause
     }
 
     fn read(&mut self) -> IoTaskStatus {
         let mut buf = self.ctx.get_read_buf();
 
         let fd = self.fd();
-        let mut total = 0;
         loop {
             self.ctx.resize_read_buf(&mut buf);
 
@@ -416,7 +430,6 @@ impl StreamItem {
 
             let res = match syscall!(break libc::read(fd, chunk_ptr.cast(), chunk_len)) {
                 Poll::Ready(Ok(size)) => {
-                    total += size;
                     if size > 0 {
                         unsafe { buf.advance_mut(size) };
                         continue;
@@ -426,8 +439,12 @@ impl StreamItem {
                 Poll::Ready(Err(err)) => Poll::Ready(Err(Some(err))),
                 Poll::Pending => Poll::Pending,
             };
-            log::trace!("{}: {fd:?}-Rdt sz({total:?}) = {res:?}", self.tag());
-            return self.ctx.release_read_buf(total, buf, res);
+            log::trace!(
+                "{}: {fd:?}-Rdt sz({:?}) = {res:?}",
+                self.tag(),
+                buf.newbytes()
+            );
+            return self.ctx.release_read_buf(buf, res);
         }
     }
 }

ntex-tls/src/rustls/stream.rs
@@ -2,8 +2,8 @@ use std::cell::RefCell;
 use std::io::{self, Read, Write};
 use std::{any, cmp, future::poll_fn, ops::Deref, ops::DerefMut, task::Poll, task::ready};
 
-use ntex_bytes::BufMut;
-use ntex_io::{Io, ReadBuf, WriteBuf, types};
+use ntex_bytes::{BufMut, BytePages, BytesMut};
+use ntex_io::{FilterBuf, Io, types};
 use tls_rustls::{ConnectionCommon, SideData};
 
 use super::{PeerCert, PeerCertChain};
@@ -59,71 +59,80 @@ where
         }
     }
 
-    pub(crate) fn process_read_buf(&mut self, buf: &ReadBuf<'_>) -> io::Result<usize> {
-        let mut new_bytes = 0;
-
+    pub(crate) fn process_read_buf(&mut self, buf: &mut FilterBuf<'_>) -> io::Result<()> {
         // get processed buffer
-        buf.with_src(|src| {
-            if let Some(src) = src {
-                buf.with_dst(|dst| {
-                    loop {
-                        let mut cursor = io::Cursor::new(&src);
-                        let n = match self.session.read_tls(&mut cursor) {
-                            Ok(n) => n,
-                            Err(ref err) if err.kind() == io::ErrorKind::WouldBlock => {
-                                break;
-                            }
-                            Err(err) => return Err(err),
-                        };
-                        src.advance_to(n);
-                        let state = self
-                            .session
-                            .process_new_packets()
-                            .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
+        buf.with_buffers(|io, r_src, r_dst, _, _| {
+            if let Some(src) = r_src {
+                loop {
+                    match self.session.read_tls(src) {
+                        Ok(_) => {}
+                        Err(ref err) if err.kind() == io::ErrorKind::WouldBlock => {
+                            break;
+                        }
+                        Err(err) => return Err(err),
+                    }
+                    let state = self
+                        .session
+                        .process_new_packets()
+                        .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
 
-                        let new_b = state.plaintext_bytes_to_read();
-                        if new_b > 0 {
+                    let new_b = state.plaintext_bytes_to_read();
+                    if new_b > 0 {
+                        if r_dst.is_none() {
+                            *r_dst = Some(io.get_read_buf());
+                        }
+                        if let Some(dst) = r_dst {
                             dst.reserve(new_b);
+
                             let chunk: &mut [u8] =
                                 unsafe { &mut *(&raw mut *dst.chunk_mut() as *mut [u8]) };
                             let v = self.session.reader().read(chunk)?;
                             unsafe { dst.advance_mut(v) };
-                            new_bytes += v;
-                        } else if src.is_empty() {
-                            break;
                         }
+                    } else if src.is_empty() {
+                        break;
                     }
-                    Ok::<_, io::Error>(())
-                })?;
+                }
+                Ok::<_, io::Error>(())
+            } else {
+                Ok(())
             }
-            Ok(new_bytes)
         })
     }
 
-    pub(crate) fn process_write_buf(&mut self, buf: &WriteBuf<'_>) -> io::Result<()> {
-        buf.with_src(|src| {
-            if let Some(src) = src {
-                let mut io = Wrapper(buf);
-
-                'outer: loop {
-                    if !src.is_empty() {
-                        src.advance_to(self.session.writer().write(src)?);
-
-                        loop {
-                            match self.session.write_tls(&mut io) {
-                                Ok(0) => continue 'outer,
-                                Ok(_) => (),
-                                Err(ref err) if err.kind() == io::ErrorKind::WouldBlock => {
-                                    break;
-                                }
-                                Err(err) => return Err(err),
-                            }
-                        }
+    pub(crate) fn process_write_buf(&mut self, buf: &mut FilterBuf<'_>) -> io::Result<()> {
+        buf.with_buffers(|_, r_src, _, w_src, w_dst| {
+            loop {
+                // write to tls stream
+                while let Some(mut page) = w_src.take() {
+                    page.advance_to(self.session.writer().write(&page)?);
+                    if !w_src.prepend(page) {
+                        // whole buffer is consumed, write next one
+                        continue;
                     }
                     break;
                 }
+
+                // write tls records to output buffer
+                if self.session.wants_write() {
+                    let mut io = Wrapper { r_src, w_dst };
+                    loop {
+                        match self.session.write_tls(&mut io) {
+                            Ok(n) => {
+                                if n == 0 {
+                                    break;
+                                }
+                            }
+                            Err(ref err) if err.kind() == io::ErrorKind::WouldBlock => {
+                                break;
+                            }
+                            Err(err) => return Err(err),
+                        }
+                    }
+                    continue;
+                }
+                return Ok(());
             }
-            Ok(())
         })
     }
 }
@@ -139,36 +148,36 @@ where
     loop {
         let handshaking = io.with_buf(|buf| {
             let mut session = session.borrow_mut();
-            let mut wrp = Wrapper(buf);
+            buf.with_buffers(|_, r_src, _, _, w_dst| {
+                let mut io = Wrapper { r_src, w_dst };
 
-            while session.wants_read() {
-                let has_data = buf.with_read_buf(|rbuf| {
-                    rbuf.with_src(|b| b.as_ref().is_some_and(|b| !b.is_empty()))
-                });
+                while session.wants_read() {
+                    let has_data = io.r_src.as_ref().is_some_and(|b| !b.is_empty());
 
-                if has_data {
-                    if session.read_tls(&mut wrp)? == 0 {
-                        return Err(io::Error::new(
-                            io::ErrorKind::NotConnected,
-                            "disconnected",
-                        ));
+                    if has_data {
+                        if session.read_tls(&mut io)? == 0 {
+                            return Err(io::Error::new(
+                                io::ErrorKind::NotConnected,
+                                "disconnected",
+                            ));
+                        }
+                        session.process_new_packets().map_err(|err| {
+                            // In case we have an alert to send describing this error,
+                            // try a last-gasp write -- but don't predate the primary
+                            // error.
+                            let _ = session.write_tls(&mut io);
+                            io::Error::new(io::ErrorKind::InvalidData, err)
+                        })?;
+                    } else {
+                        break;
                     }
-                    session.process_new_packets().map_err(|err| {
-                        // In case we have an alert to send describing this error,
-                        // try a last-gasp write -- but don't predate the primary
-                        // error.
-                        let _ = session.write_tls(&mut wrp);
-                        io::Error::new(io::ErrorKind::InvalidData, err)
-                    })?;
-                } else {
-                    break;
                 }
-            }
 
-            while session.wants_write() {
-                session.write_tls(&mut wrp).map(|_| ())?;
-            }
-            Ok(session.is_handshaking())
+                while session.wants_write() {
+                    session.write_tls(&mut io).map(|_| ())?;
+                }
+                Ok(session.is_handshaking())
+            })
         })??;
 
         if handshaking {
@@ -186,29 +195,28 @@ where
     }
 }
 
-pub(crate) struct Wrapper<'a, 'b>(&'a WriteBuf<'b>);
+pub(crate) struct Wrapper<'a> {
+    r_src: &'a mut Option<BytesMut>,
+    w_dst: &'a mut BytePages,
+}
 
-impl io::Read for Wrapper<'_, '_> {
+impl io::Read for Wrapper<'_> {
     fn read(&mut self, dst: &mut [u8]) -> io::Result<usize> {
-        self.0.with_read_buf(|buf| {
-            buf.with_src(|buf| {
-                if let Some(buf) = buf {
-                    let len = cmp::min(buf.len(), dst.len());
-                    if len > 0 {
-                        dst[..len].copy_from_slice(&buf[..len]);
-                        buf.advance_to(len);
-                        return Ok(len);
-                    }
-                }
-                Err(io::Error::new(io::ErrorKind::WouldBlock, ""))
-            })
-        })
+        if let Some(buf) = self.r_src {
+            let len = cmp::min(buf.len(), dst.len());
+            if len > 0 {
+                dst[..len].copy_from_slice(&buf[..len]);
+                buf.advance_to(len);
+                return Ok(len);
+            }
+        }
+        Err(io::Error::new(io::ErrorKind::WouldBlock, ""))
     }
 }
 
-impl io::Write for Wrapper<'_, '_> {
+impl io::Write for Wrapper<'_> {
     fn write(&mut self, src: &[u8]) -> io::Result<usize> {
-        self.0.with_dst(|buf| buf.extend_from_slice(src));
+        self.w_dst.put_slice(src);
         Ok(src.len())
     }
 

Cargo.toml
@@ -60,9 +60,9 @@ ntex-error = "2.2.0"
 ntex-h2 = "3.9.0"
 ntex-http = "1.1.0"
 ntex-dispatcher = "3.1.0"
-ntex-io = "3.9.2"
+ntex-io = "3.10.0"
+ntex-net = "3.10.0"
 ntex-macros = "3.2.0"
-ntex-net = "3.9.1"
 ntex-router = "1.0.0"
 ntex-rt = "3.12.0"
 ntex-server = "3.9.0"

ntex-io/Cargo.toml
@@ -1,6 +1,6 @@
 [package]
 name = "ntex-io"
-version = "3.9.2"
+version = "3.10.0"
 authors = ["ntex contributors <team@ntex.rs>"]
 description = "Utilities for abstracting io streams"
 keywords = ["network", "framework", "async", "futures"]
@@ -28,6 +28,8 @@ ntex-service = { workspace = true }
 bitflags = { workspace = true }
 log = { workspace = true }
 pin-project-lite = { workspace = true }
+
+backtrace = { workspace = true }
 
 [dev-dependencies]
 ntex = { workspace = true }

commit 108ee4eda868ccce57d7226a5410040920ba2f33

Fix io_uring driver silently dropping partial-send remainder
The io-uring write path (ntex-net/src/uring/stream.rs) took one whole
page out of the write buffer and submitted it as a Send/SendZc op with
the full page length. Without MSG_WAITALL the kernel may complete a
SHORT send (n < page.len()) under ordinary write backpressure. On
completion the result was mapped to `n > 0` and the page was dropped;
`n` was never compared to `buf.len()`. The unwritten tail `buf[n..]` was
therefore silently discarded:
  - if more pages were queued, the next page was submitted, leaving a
    mid-stream hole (stream corruption);
  - otherwise the write buffer looked empty -> silent truncation.

This regressed with 6b807efa "Add support for vectorized writes (#838)".

Fix: when a send completes with n < buf.len(), advance the page by n and
prepend the remainder back to the front of the write buffer before any
further page is submitted, so the byte stream stays contiguous.
  - Plain Send arm: advance + prepend the remainder, then the normal
    update_write_status -> st.send path resubmits it.
  - SendZc notif arm: the buffer is only safe to touch once the kernel
    releases it (the notif), and the byte count was stashed in `result`
    by the data completion; advance + prepend the remainder there.
  - SendZc more arm: only eagerly submit the next chunk on a FULL send
    (n == buf.len()). On a partial send we must NOT send the next page
    here, because the remainder is only returned to the buffer in the
    notif arm; sending the next page first would jump it ahead of this
    page's remainder and corrupt the stream.

This mirrors the polling/tokio/compio drivers, which already do
advance_to(n) + prepend the unwritten remainder. Adds a driver-agnostic
regression test (test_partial_writes_under_backpressure) that forces
server-side send-buffer backpressure with a slow reader and asserts every
byte arrives intact and in order.

ntex-net/src/uring/stream.rs
@@ -280,9 +280,24 @@ impl Handler for StreamOpsHandler {
                         );
 
                         if cqueue::notif(flags) {
+                            // SendZc buffer-release notification. The kernel is
+                            // done with `buf` now, so this is the only place it
+                            // is safe to return the unwritten remainder. The data
+                            // completion (`more` arm) stashed the byte count into
+                            // `result`.
                             let res = result.unwrap_or(res).map(|n| {
                                 if n == 0 {
                                     item.ctx.stop(None);
+                                } else if n < buf.len() {
+                                    // Short send: return the unwritten tail to the
+                                    // front of the write buffer instead of dropping
+                                    // it. Mirrors the polling/tokio/compio drivers
+                                    // which `advance_to(n)` + prepend the remainder.
+                                    let mut page = buf;
+                                    page.advance_to(n);
+                                    item.ctx.with_write_buf(|wrt| {
+                                        wrt.prepend(page);
+                                    });
                                 }
                                 n > 0
                             });
@@ -293,8 +308,17 @@ impl Handler for StreamOpsHandler {
                             // reset op reference
                             item.wr_op.take();
 
-                            // try to send next chunk
-                            if res.is_ok() {
+                            // SendZc data completion. `res` carries the byte count
+                            // `n`. Only eagerly submit the next chunk on a FULL send
+                            // (`n == buf.len()`). On a partial send we must NOT send
+                            // the next page here: the unwritten remainder of `buf`
+                            // is only returned to the write buffer once the kernel
+                            // releases it in the `notif` arm above. Submitting the
+                            // next page now would jump it ahead of this page's
+                            // remainder and corrupt the stream. For a partial send
+                            // the `notif` arm prepends the remainder and the normal
+                            // `update_write_status -> st.send` path resubmits it.
+                            if matches!(res, Ok(n) if n == buf.len()) {
                                 st.send(id, &self.inner.api);
                             }
                             // insert op back for "notify" handling
@@ -308,10 +332,22 @@ impl Handler for StreamOpsHandler {
                             // reset op reference
                             item.wr_op.take();
 
-                            // release buffer and try to send next chunk
+                            // Plain (non-ZC) send single completion. Release buffer
+                            // and try to send next chunk.
                             let res = res.map(|n| {
                                 if n == 0 {
                                     item.ctx.stop(None);
+                                } else if n < buf.len() {
+                                    // Short send: return the unwritten tail to the
+                                    // front of the write buffer so the following
+                                    // `st.send` resubmits it instead of dropping it.
+                                    // Mirrors the polling/tokio/compio drivers
+                                    // (`advance_to(n)` + prepend the remainder).
+                                    let mut page = buf;
+                                    page.advance_to(n);
+                                    item.ctx.with_write_buf(|wrt| {
+                                        wrt.prepend(page);
+                                    });
                                 }
                                 n > 0
                             });

ntex/tests/connect.rs
@@ -453,3 +453,69 @@ async fn basic_connect_service() {
     let result = srv.connect(format!("{}", server.addr())).await;
     assert!(result.is_ok());
 }
+
+/// Slow reader forces the server-side socket send buffer to fill up,
+/// producing short (partial) writes on the server. Every byte must
+/// still arrive intact and in order.
+///
+/// Regression test for the io-uring driver losing the unwritten tail
+/// of partially completed send operations.
+#[ntex::test]
+async fn test_partial_writes_under_backpressure() {
+    const TOTAL: usize = 16 * 1024 * 1024;
+    const CHUNK: usize = 256 * 1024;
+
+    fn pattern_byte(pos: usize) -> u8 {
+        (pos % 251) as u8
+    }
+
+    let srv = test_server(async || {
+        fn_service(|io: Io| async move {
+            let mut data = Vec::with_capacity(TOTAL);
+            for pos in 0..TOTAL {
+                data.push(pattern_byte(pos));
+            }
+            let data = Bytes::from(data);
+
+            let mut pos = 0;
+            while pos < TOTAL {
+                let end = std::cmp::min(pos + CHUNK, TOTAL);
+                if io.send(data.slice(pos..end), &BytesCodec).await.is_err() {
+                    return Ok(());
+                }
+                pos = end;
+            }
+            let _ = io.recv(&BytesCodec).await;
+            Ok::<_, io::Error>(())
+        })
+    });
+
+    let addr = srv.addr();
+    let client = std::thread::spawn(move || {
+        use std::io::Read;
+        let mut stream = std::net::TcpStream::connect(addr).unwrap();
+        stream.set_read_timeout(Some(std::time::Duration::from_secs(10))).unwrap();
+        let mut buf = [0u8; 8192];
+        let mut total = 0;
+        let mut mismatch = None;
+        while total < TOTAL {
+            let n = match stream.read(&mut buf) {
+                Ok(0) => break,
+                Ok(n) => n,
+                Err(_) => break,
+            };
+            if mismatch.is_none() {
+                for (i, b) in buf[..n].iter().enumerate() {
+                    if *b != pattern_byte(total + i) { mismatch = Some(total + i); break; }
+                }
+            }
+            total += n;
+            std::thread::sleep(std::time::Duration::from_millis(1));
+        }
+        (total, mismatch)
+    });
+
+    let (total, mismatch) = client.join().unwrap();
+    assert_eq!(total, TOTAL, "client received {total} bytes, expected {TOTAL} (data lost)");
+    assert_eq!(mismatch, None, "received data corrupted at offset {mismatch:?}");
+}